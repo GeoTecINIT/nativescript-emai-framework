3c73095116052754594b172413db1aab33e59407
Alberto González <alberto.gonzalez@uji.es>
2020-07-13 18:26:34 +0200

    Add stop push provider task

--- src/internal/tasks/data-collection/push-based/index.ts
+++ src/internal/tasks/data-collection/push-based/index.ts
@@ -1,1 +1,2 @@
 export { StartPushProviderTask } from "./start-provider";
+export { StopPushProviderTask } from "./stop-provider";
---
+++ src/internal/tasks/data-collection/push-based/stop-provider.ts
@@ -0,0 +1,24 @@
+import { ProviderTask } from "../provider-task";
+import { PushProvider } from "../../../providers";
+
+import { pascalCase } from "../../../utils/string";
+
+import { TaskConfig, TaskParams } from "nativescript-task-dispatcher/tasks";
+import { DispatchableEvent } from "nativescript-task-dispatcher/events";
+
+export class StopPushProviderTask extends ProviderTask<PushProvider> {
+  constructor(provider: PushProvider, taskConfig?: TaskConfig) {
+    super(`stopDetecting${pascalCase(provider.provides)}Changes`, provider, {
+      ...taskConfig,
+      // Descendant classes should not declare custom output events
+      outputEventNames: [],
+    });
+  }
+
+  protected async onRun(
+    taskParams: TaskParams,
+    invocationEvent: DispatchableEvent
+  ): Promise<void> {
+    await this.provider.stopProviding();
+  }
+}
