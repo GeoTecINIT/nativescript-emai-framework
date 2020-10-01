fcbe27da5f96d99e9dc56207b17db96391d87cff
Alberto Gonzalez <algonzal@uji.es>
2020-10-01 20:03:13 +0200

    Update notification hdlr to read notification ctx

--- demo/app/notification-handler/notification-handler-page.ts
+++ demo/app/notification-handler/notification-handler-page.ts
@@ -1,5 +1,13 @@
-import { EventData } from "tns-core-modules/data/observable";
 import { View } from "tns-core-modules/ui/core/view";
+import { Page } from "tns-core-modules/ui/page";
+import { EventData, fromObject } from "tns-core-modules/data/observable";
+import { getNotificationHandlerService } from "~/notification-handler/notification-handler-service";
+
+export function onNavigatedTo(args: EventData) {
+    const context = getNotificationHandlerService().tappedNotification;
+    const page: Page = <Page>args.object;
+    page.bindingContext = fromObject(context);
+}
 
 export function closeModal(args: EventData) {
     const view: View = <View>args.object;
---
+++ demo/app/notification-handler/notification-handler-root.ts
@@ -0,0 +1,9 @@
+import { ShownModallyData } from "tns-core-modules/ui/core/view";
+import { Notification } from "nativescript-emai-framework/internal/notifications";
+import { getNotificationHandlerService } from "~/notification-handler/notification-handler-service";
+
+export function onShownModally(args: ShownModallyData) {
+    getNotificationHandlerService().tappedNotification = <Notification>(
+        args.context
+    );
+}
