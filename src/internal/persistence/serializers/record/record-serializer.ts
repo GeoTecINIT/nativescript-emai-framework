4765ead697fac8dcaa1e14d6dbff87739468be08
Miguel <al341802@uji.es>
2021-03-09 11:16:38 +0100

    Delete record serializers

--- src/internal/persistence/serializers/record/record-serializer.ts
+++
@@ -1,7 +0,0 @@
-import { Record } from "../../../providers/base-record";
-import { SerializedRecord } from "./serialized-record";
-
-export interface RecordSerializer<T extends Record> {
-  serialize(record: T): SerializedRecord;
-  deserialize(serializedRecord: SerializedRecord): T;
-}
